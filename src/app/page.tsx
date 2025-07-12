8da2b380e40e6b2eee56c866a81102b8522df955
Konbu96 <hajime1129okamoto@gmail.com>
2025-07-12 19:10:51 +0900

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>
-        noda-tandf-club
+    <div className="bg-white grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1 className="text-[50px] text-blue-900 font-bold mb-0">
+        NODA-Track&Field-club
       </h1>
+      <h2 className="text-[40px] text-blue-500 mt-0">
+        部活風景
+      </h2>
     </div>
   );
 }
