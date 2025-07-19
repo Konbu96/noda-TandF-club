32c4f4ce462370ecd5a9233ef8fe42032dc1d7e7
Konbu96 <hajime1129okamoto@gmail.com>
2025-07-19 16:40:05 +0900

    new/2025/07/19

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,31 @@
 import Image from "next/image";
+import { topPhotos } from "./photo";
 
 export default function Home() {
   return (
-    <div className="bg-white grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="bg-white text-center">
       <h1 className="text-[50px] text-blue-900 font-bold mb-0">
-        NODA-Track&Field-club
+        野田学園陸上競技部
       </h1>
-      <h2 className="text-[40px] text-blue-500 mt-0">
+
+      <section>
+        <h2 className="text-[40px] text-blue-500 mt-0">
         部活風景
-      </h2>
+        </h2>
+        <div className="grid grid-cols-3 gap-4">
+          {topPhotos.map((photo,index) => (
+          <div key={index} className="relative w-full h-60 overflow-hidden rounded-lg">
+             <Image
+            src={photo.path}
+            alt={photo.alt}
+            fill={true}
+            className="object-cover"
+           />
+          </div>
+          ))}
+        </div>
+      </section>
+      
     </div>
   );
-}
+}  
---
+++ src/app/photo.ts
@@ -0,0 +1,16 @@
+export const topPhotos = [
+    {
+      path:'/img/image1.jpg',
+      alt:'冬季練習'
+    },
+    {
+      path:'/img/image2.jpg',
+      alt:'部活後の水あそび'
+    },{
+      path:'/img/image3.jpg',
+      alt:'補助競技場'
+    },{
+      path:'/img/image4.jpg',
+      alt:'お菓子パーティ'
+    },
+];
