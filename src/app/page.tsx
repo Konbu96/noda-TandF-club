26b1ddb9cc3e29b8edebf35216bc1dc3409f563d
Konbu96 <hajime1129okamoto@gmail.com>
2025-08-02 17:05:26 +0900

    side update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,57 @@
 import Image from "next/image";
 import { topPhotos } from "./photo";
+import Link from "next/link";
+
+const menus = [
+  {
+    label: '概要',
+    href: '/#intro',
+    isExternal: false,
+  },
+  {
+    label: 'ニュース',
+    href: '/#news',
+    isExternal: false,
+  },
+  {
+    label: 'スケジュール',
+    href: '/#schedule',
+    isExternal: false,
+  },
+  {
+    label: 'instagram',
+    href: 'https://www.instagram.com/noda_track.field?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==',
+    isExternal: true,
+  },
+  {
+    label: 'NEKONIWA',
+    href: 'https://neko-niwa.com/',
+    isExternal: true,
+  },
+]
 
 export default function Home() {
   return (
-    <div className="bg-white text-center flex h-screen">
+    <div className="bg-white text-center flex h-screen text-white">
       {/* サイドメニュー */}
         <aside className="w-70 bg-blue-900 p-4">
-          <h1 className="leading-tight text-[10px] sm:text-[35px] text-white font-bold m-2">
+          <h1 className="leading-tight text-[10px] sm:text-[35px] font-bold m-2">
         野田学園<br/>陸上競技部
           </h1>
           <div>
             <ul className="text-[20px] leading-15">
-              <li><a href="">概要</a></li>
-              <li><a href="">ニュース</a></li>
-              <li><a href="">スケジュール</a></li>
-              <li><a href="https://www.instagram.com/noda_track.field?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank">instagram</a></li>
-              <li><a href="https://neko-niwa.com/" target="_blank">NEKONIWA</a></li>
+              {menus.map((menu, index) => (
+                <li key={index} className="mb-4">
+                  <Link 
+                    href={menu.href}
+                    target={menu.isExternal ? '_blank' : '_self'}
+                    rel={menu.isExternal ? 'noopener noreferrer' : undefined}
+                    className="text-white hover:text-gray-500"
+                  >
+                    {menu.label}
+                  </Link>
+                </li>
+              ))}
             </ul>
           </div>
         </aside>
